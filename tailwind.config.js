760e652928a2577d93122c5122b663151cf1f5e5
Annabelle <annabelle.braye@gmail.com>
2023-09-29 17:33:00 +0200

    dist(style.css): remove dist from gitignore, add content to tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
+  content: [
+    './*.{html,js}',
+    './javascript/*.{html,js}',
+    './javascript/factories/*.{html,js}',
+  ],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
